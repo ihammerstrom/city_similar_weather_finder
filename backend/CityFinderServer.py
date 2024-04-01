9a50b85a6f254805e05622105eb8f6ae52c4e5bb
Ian Hammerstrom <ihammerstrom@ian-air.local>
2024-04-01 12:25:49 -0700

    initial server commit

---
+++ backend/CityFinderServer.py
@@ -0,0 +1,37 @@
+from flask import Flask, current_app, request
+from CityFinder import CityFinder
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World! What city would you like to compare?'
+
+
+@app.route('/get_city')
+def query_example():
+    # Get the 'name' query parameter from the request
+    name = request.args.get('city_name')
+
+    print("ok")
+    if name:
+        city_finder = current_app.config.get('city_finder')
+        print("ok2")
+        return city_finder.get_similar_cities(name, 3)
+    else:
+        return 'Hello, what\'s your name?'
+
+
+@app.route('/get_city_list')
+def get_city_list():
+    city_finder = current_app.config.get('city_finder')
+    return city_finder.get_city_list()
+
+
+if __name__ == '__main__':
+    # Initialize the variable when the application starts
+    with app.app_context():
+        current_app.config['city_finder'] = CityFinder("test_data/")
+
+    app.run(debug=True)

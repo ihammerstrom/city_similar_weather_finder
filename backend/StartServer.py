4e1dc1f6b918bd6919d27c19ce571a27a362732d
Ian Hammerstrom <ihammerstrom@ian-air.local>
2024-04-11 16:45:31 -0700

    changed CORS settings

--- backend/StartServer.py
+++ backend/StartServer.py
@@ -7,7 +7,8 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(api_v1, url_prefix='/api/v1')
-    CORS(app)
+    CORS(app, origins=["http://localhost:3000", "https://cityfinder.org"], headers=['Content-Type'],
+        expose_headers=['Access-Control-Allow-Origin'], supports_credentials=True)
     with app.app_context():
         current_app.config['city_finder'] = CityFinder()
     return app

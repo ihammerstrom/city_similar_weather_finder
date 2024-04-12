95ade111f1888504b035551b387311d589802fef
Ian Hammerstrom <ihammerstrom@ian-air.local>
2024-04-12 11:44:33 -0700

    changed chart height to work better on mobile

--- frontend/src/CityWeatherGraph.tsx
+++ frontend/src/CityWeatherGraph.tsx
@@ -51,36 +51,36 @@ const CityWeatherGraph: React.FC<CityWeatherGraphProps> = ({ data, backgroundDat
       {
         label: '(origin) Average Temperature (°C)',
         data: backgroundData.TAVG,
-        borderColor: 'rgb(255, 99, 132, 0.1)', // Red
-        backgroundColor: 'rgba(255, 99, 132, 0.1)',
+        borderColor: 'rgb(255, 99, 132, 0.15)', // Red
+        backgroundColor: 'rgba(255, 99, 132, 0.15)',
         yAxisID: 'y', // Assign to the first Y axis
       },
       {
         label: '(origin) Precipitation (mm)',
         data: backgroundData.PRCP,
-        borderColor: 'rgb(54, 162, 235, 0.1)', // Blue
-        backgroundColor: 'rgba(54, 162, 235, 0.1)',
+        borderColor: 'rgb(54, 162, 235, 0.15)', // Blue
+        backgroundColor: 'rgba(54, 162, 235, 0.15)',
         yAxisID: 'y1', // Assign to the second Y axis
       },
       {
         label: '(origin) Snowfall (mm)',
         data: backgroundData.SNOW,
-        borderColor: 'rgb(75, 192, 192, 0.1)', // Green
-        backgroundColor: 'rgba(75, 192, 192, 0.1)',
+        borderColor: 'rgb(75, 192, 192, 0.15)', // Green
+        backgroundColor: 'rgba(75, 192, 192, 0.15)',
         yAxisID: 'y1',
       },
       {
         label: '(origin) Max Temperature (°C)',
         data: backgroundData.TMAX,
-        borderColor: 'rgb(255, 205, 86, 0.1)', // Yellow
-        backgroundColor: 'rgba(255, 205, 86, 0.1)',
+        borderColor: 'rgb(255, 205, 86, 0.15)', // Yellow
+        backgroundColor: 'rgba(255, 205, 86, 0.15)',
         yAxisID: 'y', // Assign to the first Y axis
       },
       {
         label: '(origin) Min Temperature (°C)',
         data: backgroundData.TMIN,
-        borderColor: 'rgb(153, 102, 255, 0.1)', // Purple
-        backgroundColor: 'rgba(153, 102, 255, 0.1)',
+        borderColor: 'rgb(153, 102, 255, 0.15)', // Purple
+        backgroundColor: 'rgba(153, 102, 255, 0.15)',
         yAxisID: 'y', // Assign to the first Y axis
       },
 
@@ -95,6 +95,7 @@ const CityWeatherGraph: React.FC<CityWeatherGraphProps> = ({ data, backgroundDat
         text: `${data.name} ${data.similarity.toPrecision(5)}`, // Use city name as chart title
       },
     },
+    maintainAspectRatio: false,
     scales: {
       y: {
         beginAtZero: true,
@@ -103,7 +104,9 @@ const CityWeatherGraph: React.FC<CityWeatherGraphProps> = ({ data, backgroundDat
   };
 
   return (
-    <Line data={chartData} options={options} />
+    <div style={{height: '500px'}}>
+      <Line data={chartData} options={options} />
+    </div>
   );
 };
 
